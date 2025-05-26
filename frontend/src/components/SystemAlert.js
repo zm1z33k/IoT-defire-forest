d4185e74ef2f4235669828f0e1cd0fe5bd7e20b9
Daria <darianaivanova206@gmail.com>
2025-05-26 20:41:54 +0200

    Change style

--- frontend/src/components/SystemAlert.js
+++ frontend/src/components/SystemAlert.js
@@ -8,7 +8,7 @@ const SystemAlert = () => {
   const [alerts, setAlerts] = useState([]);
 
    useEffect(() => {
-    axios.get('http://wildfireeye.onrender.com/api/firebasedata/alerts')
+    axios.get('https://wildfireeye.onrender.com/api/firebasedata/alerts')
       .then(res => setAlerts(res.data))
       .catch(err => console.error(err));
   }, []);
